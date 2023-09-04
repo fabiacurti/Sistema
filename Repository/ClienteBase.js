c307bb9e7b1bf0e41ab1ea1f72e59006845f8460
DESKTOP-J2463JH\fcurt <fcurti1998@outlook.com>
2023-09-03 22:35:59 -0300

    ClienteBase ajuste no import

--- Repository/ClienteBase.js
+++ Repository/ClienteBase.js
@@ -1,26 +1,29 @@
 import Cliente from '../Models/Cliente.js'
-import conectar from '../Conexao.js'
+import conectar from '../Repository/Conexao.js'
 export default class ClienteBase {
-
     async incluir(cliente) {
         if (cliente instanceof Cliente) {
-            const conexao = await conectar();
-            const sql = 'INSERT INTO cliente(cpf,nome,endereco,bairro,cidade,uf,telefone,email)\
-            VALUES (?,?,?,?,?,?,?,?,?)'
-            const valores = [cliente.cpf, cliente.nome, cliente.endereco, cliente.bairro,
-            cliente.cidade, cliente.uf, cliente.telefone, cliente.email]
-            await conexao.query(sql.valores);
+            try {
+                const conexao = await conectar();
+                const sql = 'INSERT INTO cliente(cpf,nome,endereco,bairro,cidade,uf,telefone,email) VALUES (?,?,?,?,?,?,?,?,?)';
+                const valores = [cliente.cpf, cliente.nome, cliente.endereco, cliente.bairro, cliente.cidade, cliente.uf, cliente.telefone, cliente.email];
+                await conexao.query(sql, valores);
+                // Feche a conexão após a operação
+                await conexao.end();
+            } catch (error) {
+                console.error('Erro ao incluir cliente:', error);
+            }
         }
-
     }
+
 
     async alterar(cliente) {
         if (cliente instanceof Cliente) {
             const conexao = await conectar();
             const sql = 'UPDATE cliente SET nome=?,endereco=?,bairro=?,cidade=?,uf=?,telefone=?,email=?\
             WHERE cpf=?'
-            const valores = [ cliente.nome, cliente.endereco, cliente.bairro,
-            cliente.cidade, cliente.uf, cliente.telefone, cliente.email,cliente.cpf]
+            const valores = [cliente.nome, cliente.endereco, cliente.bairro,
+            cliente.cidade, cliente.uf, cliente.telefone, cliente.email, cliente.cpf]
             await conexao.query(sql.valores);
         }
     }
@@ -37,13 +40,13 @@ export default class ClienteBase {
     async consultar(termo) {
         const conexao = await conectar();
         const sql = 'SELECT * FROM clientes WHERE nome LIKE ?'
-        valores= ['%' + termo + '&'];
-        const [rows] = await  conexao.query(sql,valores);
-        const listaClientes =[];
-        for (const row in rows){
-              const cliente =new Cliente(row['cpf'],row['nome'],
-              row['endereco'],row['bairro'],row['cidade'],row['uf'],row['telefone'],row['email']);
-            listaClientes=push(cliente);
+        valores = ['%' + termo + '&'];
+        const [rows] = await conexao.query(sql, valores);
+        const listaClientes = [];
+        for (const row in rows) {
+            const cliente = new Cliente(row['cpf'], row['nome'],
+                row['endereco'], row['bairro'], row['cidade'], row['uf'], row['telefone'], row['email']);
+            listaClientes = push(cliente);
         }
         return listaClientes;
 
@@ -51,13 +54,13 @@ export default class ClienteBase {
     async consultarCPF(cpf) {
         const conexao = await conectar();
         const sql = 'SELECT * FROM clientes WHERE nome cpf =?';
-        valores= [cpf];
-        const [rows] = await  conexao.query(sql,valores);
-        const listaClientes =[];
-        for (const row in rows){
-              const cliente =new Cliente(row['cpf'],row['nome'],
-              row['endereco'],row['bairro'],row['cidade'],row['uf'],row['telefone'],row['email']);
-            listaClientes=push(cliente);
+        valores = [cpf];
+        const [rows] = await conexao.query(sql, valores);
+        const listaClientes = [];
+        for (const row in rows) {
+            const cliente = new Cliente(row['cpf'], row['nome'],
+                row['endereco'], row['bairro'], row['cidade'], row['uf'], row['telefone'], row['email']);
+            listaClientes = push(cliente);
         }
         return listaClientes;
     }
