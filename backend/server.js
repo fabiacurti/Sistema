d11d7f076f1808753cebedbbae5d52fc947f76fc
DESKTOP-J2463JH\fcurt <fcurti1998@outlook.com>
2024-03-25 21:20:57 -0300

    identação

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 
-const express =require('express')
+const express = require('express')
 const cors = require('cors');
-const app= express()
+const app = express()
 const port = 3001;
 
 
 
 
-const alunoprofessorRoutes =require('./routes/AlunoProfessorRoutes.js')
+const alunoprofessorRoutes = require('./routes/AlunoProfessorRoutes.js')
 const autorRoutes = require('./routes/autorRoutes')
 const generosRoute = require("./routes/generosRoute");
 const cadLivroRouters = require('./routes/cadLivroRouters.js')
@@ -16,23 +16,23 @@ const tipoLivroRoutes = require('./routes/tipoLivroRoutes')
 const registroEmprestimo = require('./routes/registroEmprestimoRoutes')
 const alunoRoutes = require('./routes/alunoRoutes')
 app.use(express.json())
-app.use(cors({origin:'http://localhost:3000',credentials:true}))
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
 
 
 
-app.use('/Aluno&Professor',alunoprofessorRoutes)
-app.use('/autor',autorRoutes)
-app.use('/genero',generosRoute)
+app.use('/Aluno&Professor', alunoprofessorRoutes)
+app.use('/autor', autorRoutes)
+app.use('/genero', generosRoute)
 app.use('/cadLivro', cadLivroRouters)
-app.use('/editoras',EditorasRoutes)
-app.use('/tipoLivro',tipoLivroRoutes)
-app.use('/registroEmprestimo',registroEmprestimo)
-app.use('/aluno',alunoRoutes)
+app.use('/editoras', EditorasRoutes)
+app.use('/tipoLivro', tipoLivroRoutes)
+app.use('/registroEmprestimo', registroEmprestimo)
+app.use('/aluno', alunoRoutes)
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Rodando na porta ${port}`)
 })
 
