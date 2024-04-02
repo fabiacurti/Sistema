f9e553903d3cba2a573a021b7752d2ee691c22c1
DESKTOP-J2463JH\fcurt <fcurti1998@outlook.com>
2024-04-02 18:31:38 -0300

    fix: rotas fix

--- backend/routes/alunoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const alunoController = require('../controller/alunoController');
-const router = express.Router()
-const alunoControlleres = new alunoController();
-
-router.get('/',alunoControlleres.getLista)
-
-module.exports = router;
